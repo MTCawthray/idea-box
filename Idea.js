23ed5dd42b0b2fcb551971dca373e221a674082b
Raquel Hill <raquelturing@gmail.com>
2021-06-02 16:13:52 -0600

    name variables for buttons

---
+++ Idea.js
@@ -0,0 +1,7 @@
+var filterStarIdeaBtn = document.querySelector(#filter-ideas-button);
+var saveIdeaBtn = document.querySelector(.save-input);
+var showStarIdeaBtn = document.querySelector(#show-starred-ideas-button);
+
+var bodyInput = document.querySelector(#body-input);
+var searchIdeasInput = document.querySelector(#search-ideas-box);
+var titleInput = document.querySelector(#title-input);
