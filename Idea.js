1ebf6e74459a7c1798318e3c29180cdfbe29a2e7
Raquel Hill <raquelturing@gmail.com>
2021-06-02 16:21:29 -0600

    Create event listeners for previous buttons

--- Idea.js
+++ Idea.js
@@ -5,3 +5,11 @@ var showStarIdeaBtn = document.querySelector(#show-starred-ideas-button);
 var bodyInput = document.querySelector(#body-input);
 var searchIdeasInput = document.querySelector(#search-ideas-box);
 var titleInput = document.querySelector(#title-input);
+
+
+filterStarIdeaBtn.addEventListener('', ):
+saveIdeaBtn.addEventListener('', ):
+showStarIdeaBtn.addEventListener('', ):
+bodyInput.addEventListener('', ):
+searchIdeasInput.addEventListener('', ):
+titleInput.addEventListener('', ):
