636555ca9f2f2a82ff3b09a2d1d27f838c55b8b5
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-05 17:45:17 -0600

    Move the get ideas from local storage method to our main javascript file

--- Idea.js
+++ Idea.js
@@ -7,15 +7,9 @@ class Idea {
   }
 
   saveToStorage() {
-    //pull from ideas array for value below
-    //create a for loop to itterate through ideas array
-    //at each index, we should stringify and set to local saveToStorage
-
     var stringifiedNewIdea = JSON.stringify(this);
     var stringifiedId = JSON.stringify(this.id);
     localStorage.setItem(stringifiedId, stringifiedNewIdea);
-    console.log("local storage -->", localStorage)
-//push saved instance to ideas array
   }
 
   deleteFromStorage() {
@@ -26,23 +20,8 @@ class Idea {
 
   }
 
-  getIdeasFromLocalStorage() {
-    // var retrieveIdeas = localStorage.getItem('idea');
-    ideasList = [];
-    if (localStorage) {
-      for(var i =0; i < localStorage.length; i++){
-        var retrieveIdea = localStorage.getItem(localStorage.key(i));
-        var parsedIdea = JSON.parse(retrieveIdea);
-        ideasList.push(parsedIdea);
 }
-//localStorage.length will be helpful here
-//localStorage.key can help
-//will have to get them, parse them, and know the key
-//check out mdn doc on localStorage.key for more details
 
-  }
 
-}
-}
 
 // module.exports = Idea;
