0551696c0030793fcb3b6d4a2b533d16ccbecbf6
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-05 14:26:12 -0600

    Create get ideas from local storage function that updates the global ideas list variable to include items from local storage

--- Idea.js
+++ Idea.js
@@ -10,10 +10,11 @@ class Idea {
     //pull from ideas array for value below
     //create a for loop to itterate through ideas array
     //at each index, we should stringify and set to local saveToStorage
-    // var newIdea = new Idea(titleInput.value, bodyInput.value);
-    var stringifiedNewIdea = JSON.stringify(newIdea);
-    localStorage.setItem('idea', stringifiedNewIdea);
-    console.log(localStorage)
+
+    var stringifiedNewIdea = JSON.stringify(this);
+    var stringifiedId = JSON.stringify(this.id);
+    localStorage.setItem(stringifiedId, stringifiedNewIdea);
+    console.log("local storage -->", localStorage)
 //push saved instance to ideas array
   }
 
@@ -24,7 +25,24 @@ class Idea {
   updateIdea() {
 
   }
-}
 
+  getIdeasFromLocalStorage() {
+    // var retrieveIdeas = localStorage.getItem('idea');
+    ideasList = [];
+    if (localStorage) {
+      for(var i =0; i < localStorage.length; i++){
+        var retrieveIdea = localStorage.getItem(localStorage.key(i));
+        var parsedIdea = JSON.parse(retrieveIdea);
+        ideasList.push(parsedIdea);
+}
+//localStorage.length will be helpful here
+//localStorage.key can help
+//will have to get them, parse them, and know the key
+//check out mdn doc on localStorage.key for more details
+
+  }
+
+}
+}
 
 // module.exports = Idea;
