8651e2c0ad37c2c957a0dfcc9d808c6d02048e7f
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-05 11:46:17 -0600

    Add psuedocode to flush out construction ideas

--- Idea.js
+++ Idea.js
@@ -1,13 +1,16 @@
 class Idea {
-  constructor(title, body) {
-    this.title = title;
-    this.body = body;
+  constructor(ideaDetails) {
+    this.title = ideaDetails.title;
+    this.body = ideaDetails.body;
     this.star = false;
     this.id = new Date().valueOf();
   }
 
   saveToStorage() {
-    var newIdea = new Idea(titleInput.value, bodyInput.value);
+    //pull from ideas array for value below
+    //create a for loop to itterate through ideas array
+    //at each index, we should stringify and set to local saveToStorage
+    // var newIdea = new Idea(titleInput.value, bodyInput.value);
     var stringifiedNewIdea = JSON.stringify(newIdea);
     localStorage.setItem('idea', stringifiedNewIdea);
     console.log(localStorage)
