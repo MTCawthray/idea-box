6fb538abf3ca5f748fe242e830fbb3c5ad3b09cd
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-04 19:07:57 -0600

    Create outline of how we should construct the class constructor and methods

--- Idea.js
+++ Idea.js
@@ -2,8 +2,24 @@ class Idea {
   constructor(title, body, star, id) {
     this.title = title;
     this.body = body;
-    this.
+    this.star = star;
+    this.id = id;
   }
+
+  saveToStorage() {
+//create a variable that stores our object instance
+//create a variable that stores the var above converted to JSON string
+//set
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateIdea() {
+
+  }
+
 }
 
 module.exports = Idea;
