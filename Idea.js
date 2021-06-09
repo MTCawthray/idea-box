f2b7e865f43c45daa57d1d9d33437b346e76d5f9
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-08 18:43:43 -0600

    Refactor update idea method to account for active and inactive star types

--- Idea.js
+++ Idea.js
@@ -2,7 +2,7 @@ class Idea {
   constructor(ideaDetails) {
     this.title = ideaDetails.title;
     this.body = ideaDetails.body;
-    this.star = ideaDetails.star || false;
+    this.star = false;
     this.id = ideaDetails.id || new Date().valueOf();
   }
   saveToStorage() {
@@ -17,23 +17,22 @@ class Idea {
     }
   }
   updateIdea(event) {
-    debugger
     var idea;
-    console.log('inside updateIdea', event)
     if (event.target.className === 'star active') {
       this.star = true;
-      //update local saveToStorage
       idea = localStorage.getItem(this.id);
       var parsedIdea = JSON.parse(idea);
-      console.log(parsedIdea.star);
       parsedIdea.star = true;
       var stringifiedIdea = JSON.stringify(parsedIdea);
       localStorage.setItem(this.id, stringifiedIdea);
-// check if local storage is update, if not- why?
-// reassess what arguments i'm passing in set item.
       console.log("inside updateIdea in class", this.star);
     } else if (event.target.className === 'star') {
       this.star = false;
+      idea = localStorage.getItem(this.id);
+      var parsedIdea = JSON.parse(idea);
+      parsedIdea.star = false;
+      var stringifiedIdea = JSON.stringify(parsedIdea);
+      localStorage.setItem(this.id, stringifiedIdea);
     }
   }
 };
