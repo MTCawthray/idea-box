1f04ecc838508e562aceac7132163aa6fb86c702
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-08 20:26:37 -0600

    Go through and clean up code to match style guide

--- Idea.js
+++ Idea.js
@@ -5,17 +5,20 @@ class Idea {
     this.star = false;
     this.id = ideaDetails.id || new Date().valueOf();
   }
+
   saveToStorage() {
     var stringifiedNewIdea = JSON.stringify(this);
     var stringifiedId = JSON.stringify(this.id);
     localStorage.setItem(stringifiedId, stringifiedNewIdea);
   }
+
   deleteFromStorage(event) {
     if (event.target.className === 'close-card') {
       localStorage.removeItem(event.target.id);
       renderIdea();
     }
   }
+  
   updateIdea(event) {
     var idea;
     if (event.target.className === 'star active') {
@@ -25,7 +28,6 @@ class Idea {
       parsedIdea.star = true;
       var stringifiedIdea = JSON.stringify(parsedIdea);
       localStorage.setItem(this.id, stringifiedIdea);
-      console.log("inside updateIdea in class", this.star);
     } else if (event.target.className === 'star') {
       this.star = false;
       idea = localStorage.getItem(this.id);
